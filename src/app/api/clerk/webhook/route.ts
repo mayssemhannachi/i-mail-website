3856efb35281d6758d17c10a33cc736a3c466e90
mayssemhannachi <h.mayssem2003@gmail.com>
2024-11-01 17:48:02 +0100

    Implement user creation from Clerk webhook data

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,8 +1,28 @@
 // /api/clerk/webhook
 
+import { db } from "../../../../server/db";
+
 export const POST = async (req : Request) => {
     const {data} = await req.json()
     console.log('clerk wehbook received', data)
+    const emailAdress = data.email_addresses[0].email_address
+    const firstName = data.first_name
+    const lastName = data.last_name
+    const imageUrl = data.image_url
+    const id = data.id
+
+    // Save the user to your database
+    await db.user.create({
+        data:{
+            id : id,
+            emailAdress : emailAdress,
+            firstName : firstName,
+            lastName : lastName,
+            imageUrl : imageUrl
+        }
+    })
+
+    console.log('user created')
     
     return new Response('wehbook received', {status:200})
 }
