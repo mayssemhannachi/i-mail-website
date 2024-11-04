32e4d380d61640d139dc1581c799959d8e15ab5b
mayssemhannachi <h.mayssem2003@gmail.com>
2024-11-04 10:57:26 +0100

    Google Account Linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+import React from 'react'
+import LinkAccountButton from "../components/link-account-button";
 
 export default async function Home() {
-  return<h1 className="text-red-600">hello world</h1>
-  
+  return<LinkAccountButton />
 }
