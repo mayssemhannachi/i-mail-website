2f52f664bdfc574e8e086c36fbee031f1aa1f77a
mayssemhannachi <h.mayssem2003@gmail.com>
2024-11-04 14:19:10 +0100

    testing with reading scope only

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -8,7 +8,7 @@ export const getAurinkoAuthUrl = async (serviceType : 'Google' | 'Office365' ) =
     const params = new URLSearchParams({
         clientId: process.env.AURINKO_CLIENT_ID as string,
         serviceType,
-        scopes: 'Mail.Read Mail.Send Mail.ReadWrite Mail.Drafts Mail.All',
+        scopes: 'Mail.Read ',
         responseType: 'code',
         returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`
     })
