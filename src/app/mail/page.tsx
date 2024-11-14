ff35b05c4f7f14888e3e050818f23761261a9df5
chahdatia <chahedattia28@gmail.com>
2024-11-14 22:31:07 +0100

    Begin creating shadcn email UI

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,8 +1,15 @@
+import Mail from './mail';
+/*import { Mail } from 'lucide-react';*/
 import react from 'react';
 
 const MailDashboard = () => {
   return(
-     <div>MailDashboard</div>)
+   <Mail
+    defaultLayout={[20,32,48]}
+    defaultCollapsed={false}
+    navCollapsedSize={4}
+   />
+  )
 }
 
 export default MailDashboard;
