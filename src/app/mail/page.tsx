db90fdb30e28cdb98ba4d35abd601411b139ae5e
chahdatia <chahedattia28@gmail.com>
2024-11-20 22:41:37 +0100

    Add dark mode

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -2,6 +2,8 @@
 /*import { Mail } from 'lucide-react';*/
 import React from 'react';
 import dynamic from "next/dynamic"
+import { ModeToggle } from "src/components/theme-toggle"
+
 
 const Mail = dynamic(() => {
   return import('./mail')
@@ -11,11 +13,15 @@ const Mail = dynamic(() => {
 
 const MailDashboard = () => {
   return(
-   <Mail
+    <>
+    <div className="absolute bottom-4 left-4"> <ModeToggle/> </div>
+    <Mail
     defaultLayout={[20,32,48]}
     defaultCollapsed={false}
     navCollapsedSize={4}
    />
+   </>
+   
   )
 }
 
