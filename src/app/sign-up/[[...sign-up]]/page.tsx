255bf9d262584c4ccf2f916731f1d0e75585a1ca
mayssemhannachi <h.mayssem2003@gmail.com>
2024-11-26 15:59:27 +0100

    Update layout title and icon; enhance sign-up page layout

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 export default function Page() {
-  return <SignUp />
+  return (
+  <div className='flex justify-center items-center h-screen'>
+    <SignUp />
+    </div>)
 }
